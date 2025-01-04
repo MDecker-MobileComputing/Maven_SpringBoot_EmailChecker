334994bf863114607f2182cf1d399812b091e333
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-01-04 23:02:11 +0100

    Eureka-Registry faehrt hoch https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.4.1&packaging=jar&jvmVersion=23&groupId=de.eldecker.spring.emailchecker&artifactId=registry&name=Eureka-Registry&description=Eureka-Registry%20f%C3%BCr%20Email-Checker&packageName=de.eldecker.spring.emailchecker.registry&dependencies=

---
+++ registry/src/test/java/de/eldecker/spring/emailchecker/registry/EurekaRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package de.eldecker.spring.emailchecker.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
